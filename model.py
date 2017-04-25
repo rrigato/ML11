14a49cc4124dbd78c3a3723f16c4e3642ba8c174
Ryan <rrigato@horizon.csueastbay.edu>
2017-04-24 19:33:12 -0500

    Seperate function to read in and document the data

--- model.py
+++ model.py
@@ -3,8 +3,26 @@ import pandas as pd
 import numpy as np
 class quoraModel:
 	def __init__(self):
+		'''
+			The train and test datasets contain an ID
+			and question 1/ question 2 which the model is supposed to differentiate
+			whether the questions are similar or different
+			
+
+		'''
+		self.loadData()
+
+	def loadData(self):
+		'''Loads the train/test data from a csv
+
+			self.train = Also contains a label for the ground truth
+			-0 if the questions are not the same 1 if they are
+
+		'''
 		self.train = pd.read_csv('/home/ryan/Documents/quora/train.csv')
 		self.test = pd.read_csv('/home/ryan/Documents/quora/test.csv')
+		print(self.train.head())
+		print(self.test.head())
 
 if __name__ == '__main__':
 	quoraObj = quoraModel()
