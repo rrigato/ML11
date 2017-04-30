2ed37d76015cd59bce251124779bde2123683851
Ryan <rrigato@horizon.csueastbay.edu>
2017-04-29 19:42:40 -0500

    Added a function to give an estimated logloss

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 #!usr/bin/python3
 import pandas as pd
 import numpy as np
+import nltk
+from sklearn import metrics
 class quoraModel:
 	def __init__(self):
 		'''
@@ -24,5 +26,16 @@ class quoraModel:
 		print(self.train.head())
 		print(self.test.head())
 
+	def getLogLoss(self, predictArray):
+		'''Gets the Log-loss for the training set on the test set
+			
+			This function takes an arguement of the prediction for whether the questions are a duplicate or not
+		'''
+		print('Your logloss is :')
+		print(metrics.log_loss(self.train.loc[:,'is_duplicate'], predictArray))
+		return(metrics.log_loss(self.train.loc[:,'is_duplicate'], predictArray))
+
 if __name__ == '__main__':
 	quoraObj = quoraModel()
+	
+	logLoss = quoraObj.getLogLoss([0]*quoraObj.train.shape[0])
