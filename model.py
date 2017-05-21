0ee91c4186c4364d966e18d2179c715e4a4d1de8
Ryan <rrigato@horizon.csueastbay.edu>
2017-05-21 18:19:22 -0500

    Adding the tokenizing to the full train/test

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import numpy as np
 import nltk
 from sklearn import metrics
 from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import AdaBoostClassifier
 class quoraModel:
 	def __init__(self):
 		'''
@@ -26,6 +27,37 @@ class quoraModel:
 		self.train = pd.read_csv('/home/ryan/Documents/quora/train.csv')
 		self.test = pd.read_csv('/home/ryan/Documents/quora/test.csv')
 
+
+	def getFeatures(self):
+		'''Gets some features for use in the machine learning model
+
+		'''
+
+		'''
+			Casting question1 and question2 as strings
+		'''
+		self.train.loc[:,['question1', 'question2']] = self.train.loc[:,['question1', 'question2']].astype(str)
+		self.test.loc[:,['question1', 'question2']] = self.test.loc[:,['question1', 'question2']].astype(str)
+
+
+
+	def getWordTokens(self):
+		'''Applies the word_tokenize function to train/test 
+			The word_tokenize function from the nltk module takes a sentance and returns a dictionary corresponding
+			To each space in the sentance.
+			
+			This will transform self.xTrain/self.yTrain from having a string for each question as a variable to a dictionary
+			
+			Applies for both question1 and question2
+		'''
+		
+		self.train.loc[:,'q1Token'] = self.train.loc[:,'question1'].apply(nltk.word_tokenize)
+		self.train.loc[:,'q2Token'] = self.train.loc[:,'question2'].apply(nltk.word_tokenize)
+		
+		self.test.loc[:,'q1Token'] = self.test.loc[:,'question1'].apply(nltk.word_tokenize)
+		self.test.loc[:,'q2Token'] = self.test.loc[:,'question2'].apply(nltk.word_tokenize)
+
+
 	def getTrainTest(self, percentTrain):
 		'''Splits the training dataset into train and test
 
@@ -38,37 +70,24 @@ class quoraModel:
 			ratio of the total training set to subset from
 		'''
 
-		'''
-			Casting question1 and question2 as strings
-		'''
-		self.train.loc[:,['question1', 'question2']] = self.train.loc[:,['question1', 'question2']].astype(str)
-		self.test.loc[:,['question1', 'question2']] = self.test.loc[:,['question1', 'question2']].astype(str)
 
 		'''
 			splitting into train and test
 		'''
 		self.xTrain, self.xTest, self.yTrain, self.yTest = train_test_split(self.train.loc[:, 'id':'question2'], self.train.loc[:,'is_duplicate'], train_size = percentTrain, random_state = self.RANDOM_STATE)
 
-	def getWordTokens(self):
-		'''Applies the word_tokenize function to train/test 
-			The word_tokenize function from the nltk module takes a sentance and returns a dictionary corresponding
-			To each space in the sentance.
+
+
+
+	
+	def getModel(self):
+		'''Build an adaboost model using self.xTrain
 			
-			This will transform self.xTrain/self.yTrain from having a string for each question as a variable to a dictionary
-			
-			Applies for both question1 and question2
+			self.clf = an adaboostclassifier 
 		'''
+		self.clf = AdaBoostClassifier(n_estimators=100)
+		scores = cross_val_score(self.clf, self.xTrain, self.yTrain)
 		
-		self.xTrain.loc[:,'q1Token'] = self.xTrain.loc[:,'question1'].apply(nltk.word_tokenize)
-		self.xTrain.loc[:,'q2Token'] = self.xTrain.loc[:,'question2'].apply(nltk.word_tokenize)
-		
-		self.xTest.loc[:,'q1Token'] = self.xTest.loc[:,'question1'].apply(nltk.word_tokenize)
-		self.xTest.loc[:,'q2Token'] = self.xTest.loc[:,'question2'].apply(nltk.word_tokenize)
-
-		print(self.xTest.loc[:,'q1Token':'q2Token'].head())
-		print(self.xTrain.loc[:,'q1Token':'q2Token'].head())
-
-
 	def getLogLoss(self, predictArray):
 		'''Gets the Log-loss for the training set on the test set
 			
@@ -81,8 +100,9 @@ class quoraModel:
 if __name__ == '__main__':
 	quoraObj = quoraModel()
 
-	quoraObj.getTrainTest(.75)
+	quoraObj.getFeatures()
 	
 	quoraObj.getWordTokens()
+	quoraObj.getTrainTest(.75)
 	
 	logLoss = quoraObj.getLogLoss([0]*quoraObj.yTest.shape[0])
